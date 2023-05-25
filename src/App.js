8be99e12b6501edef34837b3afb3734e2ec2cdc5
Djarlane EGBLOMASSE <Djarlane971@hotmail.fr>
2023-05-25 17:00:39 +0200

    mynetflix final

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./styles/App.css";
+import React from "react";
+import Row from "./pages/Row";
+import requests from "./api/requests";
+import Banner from "./pages/Banner";
+import Nav from "./pages/Nav";
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
